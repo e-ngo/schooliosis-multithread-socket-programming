1d412c8b1650108d47ddf2d2be88426cf976ab3e
Eric Ngo <ngoaieric@gmail.com>
2019-11-17 23:17:13 -0800

    Move client code.

--- applications/peer-to-peer-app/client.py
+++ applications/peer-to-peer-app/client.py
@@ -7,15 +7,45 @@ assignment #1.
 import socket
 import pickle
 
-class Client(object):
+class ServerDisconnect(Exception):
+    """Signals server disconnect
+
+    """
+    pass
+
+def network_exception_handler(func):
+    def wrap_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except socket.error as sock_error:
+            print(f"An HTTPError occurred: {sock_error}")
+        except ServerDisconnect:
+            print("Server has disconnected")
+        except Exception as e:
+            print(f"Something went wrong: {e}")
+    return wrap_func
+
+class Client:
+
+    BUFFER_SIZE = 65536
+    # ProxyServer constants
+    SERVER_HOST = '127.0.0.1'
+    SERVER_PORT = 12001
 
     def __init__(self):
         """
         TODO: implement this contructor
         Class contructor
         """
-        self.client_socket = None # create here your client tcp socket
+        self.init_socket()
 
+    def init_socket(self):
+        try:
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as err:
+            print("socket creation failed with error", (err))
+
+    @network_exception_handler
     def connect(self, ip_adress, port):
         """
         TODO: Implement connection to server sockets
@@ -23,8 +53,14 @@ class Client(object):
         :param port:
         :return: VOID
         """
-        pass
+        try:
+            # Connects to the server using its host IP and port
+            self.client_socket.connect((host_ip, port))
+        except OSError:
+            # socket already connected
+            pass
 
+    @network_exception_handler
     def send(self, data):
         """
         TODO: Implement client socket send method
@@ -35,15 +71,22 @@ class Client(object):
                      with pickle
         :return: VOID
         """
-        pass
+        data_serialized = pickle.dumps(data)
+        self.client_socket.send(data_serialized)
 
+    @network_exception_handler
     def recieve(self, memory_allocation_size):
         """
         TODO: implement receives data from server socket
         :param memory_allocation_size:
         :return: deserialized data
         """
-        return None
+        server_response = self.client_socket.recv(self.BUFFER_SIZE)
+        if not server_response:
+            raise ServerDisconnect()
+        # Deserializes the data.
+        server_data = pickle.loads(server_response)
+        return server_data
 
     def close(self):
         """
